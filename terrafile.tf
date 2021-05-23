d87215cb58a8cd94468f4c1c99daae3ad3099d4c
heldobarbosa <ironheldo@gmail.com>
2021-05-22 22:17:46 -0300

    NEW VPC_CLOUDSTARTKIT

--- terrafile.tf
+++ terrafile.tf
@@ -3,28 +3,28 @@ provider "aws" {
 }
 module "CloudStartKit" {
   source = "git@github.com:Accenture-Dev-private/module_vpc.git"
-  name = ""
-  cidr = ""
+  name = "CloudStartKit"
+  cidr = "10.1.0.0/16"
   azs = ""
-  enable_dns_hostnames = ""
-  enable_dns_support = ""
-  instance_tenancy = ""
-  enable_nat_gateway = ""
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  instance_tenancy = "default"
+  enable_nat_gateway = true
   external_nat_gateway_eip_id = ""
-  enable_vpn_gateway = ""
+  enable_vpn_gateway = false
   vpn_gateway_id = ""
   vpn_gateway_tags = {}
-  enable_private_route_table_propagation = ""
-  enable_public_route_table_propagation = ""
-  enable_database_route_table_propagation = ""
-  private_subnets = ["",""]
+  enable_private_route_table_propagation = false
+  enable_public_route_table_propagation = false
+  enable_database_route_table_propagation = false
+  private_subnets = ["10.1.2.0/24","10.1.3.0/24"]
   private_subnet_names = []
-  private_subnet_suffix = ""
+  private_subnet_suffix = "private-subnet"
   private_subnet_tags = {}
   name-default-sg = ""
-  public_subnets = ["",""]
+  public_subnets = ["10.1.4.0/24","10.1.5.0/24"]
   public_subnet_names = []
-  public_subnet_suffix = ""
+  public_subnet_suffix = "public-subnet"
   public_subnet_tags = {}
 #  database_subnets = []                               
 #  database_subnet_names = []                          
@@ -33,12 +33,12 @@ module "CloudStartKit" {
 #  create_db_subnet_group = true                       
 #  enable_route53_private_zone = false                 
 #  route53_private_zone_name = ""
-  allow_all_egress = ""
-  allow_extra_cidr_blocks = ["","","",""]
+  allow_all_egress = false
+  allow_extra_cidr_blocks = ["10.1.2.0/24","10.1.3.0/24","10.1.4.0/24","10.1.5.0/24"]
   allow_extra_cidr_blocks_egress = []
   peering_routes = []
   tags = {}                
-  enable_default_security_group = ""
-  default_security_group_description = ""
+  enable_default_security_group = false
+  default_security_group_description = "CloudStartKit - Default"
 
 }
